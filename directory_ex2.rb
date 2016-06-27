632d60d65c951784a331c9c5a5299e99d65b277c
Titus Iudean <iudeant@gmail.com>
2016-06-27 08:29:39 +0100

    As ch8_ex2: Print only student names which begin with a particular letter (I chose ’T’)

---
+++ directory_ex2.rb
@@ -0,0 +1,42 @@
+# store student names into an array
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # creat an empty array
+    students = []
+    #get the first name
+    name = gets.chomp
+    # while name in not empty get students in
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        puts "Hit return twice if you don't want to add more"
+        name = gets.chomp
+        puts name
+    end
+    students
+end
+
+# then print them
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def prints(students)
+  students.each_with_index do |student, index|
+    if student[:name].chars[0].upcase == "T"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(students)
+  # use the array method 'count'
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+prints(students)
+print_footer(students)
